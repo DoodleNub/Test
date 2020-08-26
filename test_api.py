dd31e159808771bfee21336ab3e7709bced0efe6
Shubham Gelera <gelerashubham@gmail.com>
2020-08-26 22:21:43 +0530

    simple version test

--- test_api.py
+++ test_api.py
@@ -5,14 +5,8 @@ import ret_dat
 app=flask.Flask('__name__')
 app.config["DEBUG"]=True
 
-@app.route('/api/v1/test',methods=['GET'])
-def try_return():
-    if 'id' in request.args:
-        id=int(request.args['id'])
-    else:
-        print("ERROR : No id passed")
-    data=ret_dat.test(id)
-    return jsonify(data)
-
+@app.route('/test',methods=['GET'])
+def home():
+    return(jsonify("THIS IS A TEST!"))
 if __name__=='__main__':
     app.run()
